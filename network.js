991cb27c6ae776492f4996c64678a7d0448c3d74
Hugo Camboulive <hugo.camboulive@gmail.com>
2009-10-23 18:48:12 +0200

    Make a mouseover event on each of the dots displayed.

--- network.js
+++ network.js
@@ -4,6 +4,7 @@ var valtomonth = {"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May
 var branchColor = ["black", "red", "blue", "green", "magenta", "cyan"];
 var xoffset = 100;
 var yoffset = 40;
+var dotsMouseOver = [];
 
 var meta;
 var data;
@@ -82,6 +83,7 @@ function drawData(ctx, data, xoffset, yoffset) {
 	ctx.fillStyle = "white";
 	ctx.fillRect(100, 40, 920 - 100, 600 - 40);
 	/* draw points */
+	dotsMouseOver = [];
 	$.each(data.commits, function(i, val) {
 		var x = 200 + 20 * val.time - xoffset - 10;
 		var y = 80 + 20 * val.space - yoffset - 10;
@@ -91,23 +93,33 @@ function drawData(ctx, data, xoffset, yoffset) {
 		if (x > 80 && x < 940 && y > 20 && y < 620) {
 			ctx.beginPath();
 			ctx.fillStyle = branchColor[(val.space-1)%6];
-			ctx.arc(x, y, 3, 0, 360, false);
+			if (val == drawDot) {
+				ctx.arc(x, y, 5, 0, 360, false);
+			} else {
+				ctx.arc(x, y, 3, 0, 360, false);
+			}
 			ctx.fill();
+			/* add the data to the array of dotsmouseover */
+			dotsMouseOver.push({"x":x, "y": y, "val": val});
 		}
 		$.each(val.parents, function(j, parnt) {
+			var xdest = 200 + 20 * parnt[1] - xoffset - 10;
+			var ydest = 80 + 20 * parnt[2] - yoffset - 10;
+			if (!needToDrawLine(x, y, xdest, ydest, 100, 40, 920, 600))
+				return;
 			if (parnt[2] == val.space) {
 				/* draw line */
 				ctx.beginPath();
 				ctx.moveTo(x - 5, y);
-				ctx.lineTo(200 + 20 * parnt[1] - xoffset - 10 + 5, y);
+				ctx.lineTo(xdest + 5, y);
 				ctx.stroke();
 			} else if (parnt[2] > val.space) {
 				/* the parent is > than the current
  				 * this will be a merge arrow */
 				ctx.beginPath();
 				ctx.strokeStyle = branchColor[(parnt[2]-1)%6];
-				ctx.moveTo(200 + 20 * parnt[1] - xoffset - 10 + 5, 80 + 20 * parnt[2] - yoffset - 10);
-				ctx.lineTo(x - 10, 80 + 20 * parnt[2] - yoffset - 10);
+				ctx.moveTo(xdest + 5, ydest);
+				ctx.lineTo(x - 10, ydest);
 				ctx.lineTo(x - 10, y + 12);
 				ctx.lineTo(x - 5, y + 5);
 				ctx.stroke();
@@ -122,9 +134,26 @@ function drawData(ctx, data, xoffset, yoffset) {
 		});
 	});
 }
+function needToDrawLine(xorig, yorig, xdest, ydest, xmin, ymin, xmax, ymax) {
+	if (xorig < xmin && xdest < xmin)
+		return false;
+	if (xorig > xmax && xdest > xmax)
+		return false;
+	if (yorig < ymin && ydest < ymin)
+		return false;
+	if (yorig > ymax && ydest > ymax)
+		return false;
+	if ( (xorig < xmin || xorig > xmax) && (ydest < ymin || ydest > ymax) )
+		return false;
+	if ( (xdest < xmin || xdest > xmax) && (yorig < ymin || yorig > ymax) )
+		return false;
+	return true;
+}
 
 var dragging = false;
 var lastPoint = {"x":0, "y":0};
+var drawDot;
+
 function mouseUp(e) {
 	dragging = false;
 }
@@ -134,9 +163,10 @@ function mouseDown(e) {
 	dragging = true;
 }
 function mouseMove(e) {
+	var end = 0;
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
 	if (dragging) {
-		var x = e.pageX - e.target.offsetLeft;
-		var y = e.pageY - e.target.offsetTop;
 		var dx = x - lastPoint.x;
 		var dy = y - lastPoint.y;
 		/* cannot scroll beyond boundaries */
@@ -155,6 +185,21 @@ function mouseMove(e) {
 		lastPoint.x = x;
 		lastPoint.y = y;
 		draw();
+	} else {
+		var found = false;
+		$.each(dotsMouseOver, function(i, val) {
+			if (found == false) {
+				if (Math.abs(val.x - x) <= 5) {
+					if (Math.abs(val.y - y) <= 5) {
+						found = val.val;
+					}
+				}
+			}
+		});
+		if (drawDot != found) {
+			drawDot = found;
+			draw();
+		}
 	}
 }
 function mouseOut(e) {
